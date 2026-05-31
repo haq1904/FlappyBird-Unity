98409ba58a4f3b764c0422ae0740f92bf136831e
Huynh Anh Quy <anhquy19042005@gmail.com>
2026-06-01 00:22:34 +0700

    feat: update background( it's can scroll and have parallax effect)

--- Assets/Scripts/Logic/Manager/BackGroundManager.cs
+++ Assets/Scripts/Logic/Manager/BackGroundManager.cs
@@ -1,21 +1,36 @@
-using DG.Tweening;
-using System.Security.Cryptography;
+
 using UnityEngine;
 
 public class BackGroundManager : MonoBehaviour
 {
     [SerializeField] private GameObject Vcam;
-    [SerializeField] private float parallaxEffect;
-    private float startPos;
+    [SerializeField] private float parallaxEffect =1;
+    [SerializeField] private float moveSpeed=1;
+    [SerializeField] private float xCoordinateToReset = -24;
+    private float startPosY;
+    private float startPosX;
 
     private void Awake()
     {
-        startPos = transform.position.y;
+        startPosX = transform.position.x;
+        startPosY = transform.position.y;
     }
 
     private void FixedUpdate()
     {
+        //Calculate distance for parallax effect
         float distance = Vcam.transform.position.y * parallaxEffect; // 0: won't move , 1: move with camera , 0.5: half;
-        transform.position = new Vector3(transform.position.x, startPos + distance, transform.position.z);
+        //Calculate finalX for moving left by x
+        float finalX = transform.position.x - (moveSpeed * Time.fixedDeltaTime);
+        //Calculate finalY for moving background with y ( parallax)
+        float finalY = startPosY + distance;
+
+        //if background reaches the point, reset position.
+        if (transform.position.x < xCoordinateToReset)
+            finalX = startPosX;
+
+        transform.position = new Vector3(finalX, finalY, transform.position.z);
+
+
     }
 }
