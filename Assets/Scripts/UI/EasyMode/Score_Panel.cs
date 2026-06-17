fd700e17be765dc4af52b6ff503aa926eb724124
Huynh Anh Quy <anhquy19042005@gmail.com>
2026-06-17 23:39:06 +0700

    feat: apply shake effect for Score_Panel by using DOShakePositio

--- Assets/Scripts/UI/EasyMode/Score_Panel.cs
+++ Assets/Scripts/UI/EasyMode/Score_Panel.cs
@@ -1,8 +1,18 @@
+using DG.Tweening;
+using JetBrains.Annotations;
 using TMPro;
 using UnityEngine;
 
 public class Score_Panel : MonoBehaviour
 {
+    [Header("Fields for shake")]
+    [SerializeField] float duration = 1;
+    [SerializeField] Vector3 strength;
+    [SerializeField] int vibrato = 1;
+    [SerializeField] float randomness = 90;
+    [SerializeField] bool snapping = false;
+    [SerializeField] bool fadeOut = true;
+    
     [Header("Game objects")]
     [SerializeField] private Animator animator;
     [SerializeField] private TextMeshProUGUI scoreText;
@@ -12,5 +22,6 @@ public class Score_Panel : MonoBehaviour
     {
         animator.Play("AddPoint",-1,0f);
         scoreText.text = point.ToString();
+        gameObject.transform.DOShakePosition(duration:duration, strength:strength, vibrato:vibrato, randomness:randomness, snapping:snapping,fadeOut:fadeOut);
     }
 }
