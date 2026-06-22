33be9bb0d83c1c57661e9f35b28631df799d26e6
Huynh Anh Quy <anhquy19042005@gmail.com>
2026-06-22 23:18:44 +0700

    feat: shake coin text

--- Assets/Scripts/UI/EasyMode/PlayerStats_Controller.cs
+++ Assets/Scripts/UI/EasyMode/PlayerStats_Controller.cs
@@ -1,5 +1,6 @@
 using DG.Tweening;
 using JetBrains.Annotations;
+using System.Xml.Serialization;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -19,15 +20,30 @@ public class PlayerStats_Controller : MonoBehaviour
     [SerializeField] private TextMeshProUGUI _coin;
     [SerializeField] private Image _coinImg;
 
+    private Vector2 _coinResetAnchoredPos;
+
+
+    private void Start()
+    {
+        _coinResetAnchoredPos = _coin.rectTransform.anchoredPosition;
+    }
 
     public void HandleChangePoint(float point)
     {
         _score.text = point.ToString();
-        transform.DOShakePosition(duration:duration, strength:strength, vibrato:vibrato, randomness:randomness, snapping:snapping,fadeOut:fadeOut);
+        transform.DOShakePosition(duration:duration, strength:strength, vibrato:vibrato, randomness:randomness, snapping:snapping,fadeOut:fadeOut).SetLink(gameObject);
     }
 
     public void HandleChangeCoin(float coin)
     {
         _coin.text = coin.ToString();
+        ShakeCoinText();
+    }
+
+    private void ShakeCoinText()
+    {
+        _coin.rectTransform.DOKill();
+        _coin.rectTransform.anchoredPosition = _coinResetAnchoredPos; 
+        _coin.rectTransform.DOShakeAnchorPos(duration: duration, strength: strength, vibrato: vibrato, randomness: randomness, snapping: snapping, fadeOut: fadeOut).SetLink(gameObject);
     }
 }
