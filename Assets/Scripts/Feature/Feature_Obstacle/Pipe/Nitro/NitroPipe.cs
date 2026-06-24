8a5c7eb883db7c71f575760a7dce0909e70df400
Huynh Anh Quy <anhquy19042005@gmail.com>
2026-06-24 12:31:00 +0700

    feat: add DORotate to nitro pipe in order to make it moving naturally after booting

--- Assets/Scripts/Feature/Feature_Obstacle/Pipe/Nitro/NitroPipe.cs
+++ Assets/Scripts/Feature/Feature_Obstacle/Pipe/Nitro/NitroPipe.cs
@@ -5,13 +5,25 @@ using DG.Tweening;
 public class NitroPipe : BasePipe
 {
     [Header("Fields")]
-    [SerializeField] private ParticleSystem _dustTop;
-    [SerializeField] private ParticleSystem _dustBot;
     [SerializeField] private float durationForBacking = 2;
     [SerializeField] private float timeSlowDown = 0.3f;
     [SerializeField] private float backDistance = 2;
     [SerializeField] private float speedForBooting = 3;
     [SerializeField] private Ease easeForBacking = Ease.Linear;
+
+    [Header("Rotate")]
+    [SerializeField] private Vector3 _desToRotate;
+    [SerializeField] private float _durationForRotating = 1;
+    [SerializeField] private Ease _easeForRotating = Ease.Linear;
+    [SerializeField] private float _amplitudeForRotating = 1.7f;
+    [SerializeField] private float _periodForRotating = 0;
+
+    [Header("Game objects")]
+    [SerializeField] private ParticleSystem _dustTop;
+    [SerializeField] private ParticleSystem _dustBot;
+    [SerializeField] private PartOfPipe _topPipe;
+    [SerializeField] private PartOfPipe _botPipe;
+
     private Sequence s;
     private bool isTrigger = false;
 
@@ -49,10 +61,13 @@ public class NitroPipe : BasePipe
             s.AppendCallback(() => { moveSpeed = 0; });
             s.Append(rb.DOMoveX((transform.position.x + backDistance), durationForBacking).SetEase(easeForBacking).SetUpdate(UpdateType.Fixed).SetLink(gameObject));
             s.AppendCallback(() => 
-            { 
+            {
+                _topPipe.transform.DORotate(_desToRotate, _durationForRotating).SetEase(_easeForRotating,_amplitudeForRotating,_periodForRotating).SetLink(gameObject);
+                _botPipe.transform.DORotate(new Vector3(_desToRotate.x, _desToRotate.y, -_desToRotate.z), _durationForRotating).SetEase(_easeForRotating, _amplitudeForRotating, _periodForRotating).SetLink(gameObject);
                 moveSpeed = takeCurrSpeed + speedForBooting;
                 _dustTop.Play();
                 _dustBot.Play();
+                
             });
             s.SetLink(gameObject, LinkBehaviour.KillOnDisable);
         }
