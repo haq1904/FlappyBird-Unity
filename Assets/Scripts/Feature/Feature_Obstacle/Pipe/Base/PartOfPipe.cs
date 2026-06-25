57834484da9146002478ab5a83e07c6d243adc78
Huynh Anh Quy <anhquy19042005@gmail.com>
2026-06-25 13:44:23 +0700

    save: save project before refactoring

--- Assets/Scripts/Feature/Feature_Obstacle/Pipe/Base/PartOfPipe.cs
+++ Assets/Scripts/Feature/Feature_Obstacle/Pipe/Base/PartOfPipe.cs
@@ -1,14 +1,23 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class PartOfPipe: MonoBehaviour
 {
-    [SerializeField] float impactForce = 1;
-    [SerializeField] float gravityScale = 1;
+    [Header("Fields")]
+    [SerializeField] private float _impactForce = 1;
+    [SerializeField] private float _gravityScale = 1;
+
+    [Header("Shake")]
+    [SerializeField] private float _durationForShaking = 1;
+    [SerializeField] private Vector3 _strengForShaking;
+    [SerializeField] private int _vibratoForShaking = 10;
+    [SerializeField] private float _randomnessForShaking = 90;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.TryGetComponent<IDamageable>(out var damageableGameObject)) {
-            damageableGameObject.TakeDamage(Vector2.left, impactForce, gravityScale);
+            damageableGameObject.TakeDamage(Vector2.left, _impactForce, _gravityScale);
+            transform.DOShakePosition(_durationForShaking, _strengForShaking, _vibratoForShaking, _randomnessForShaking,snapping : true,fadeOut : true);
         }
     }
 }
