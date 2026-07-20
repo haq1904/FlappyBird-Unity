aaf1b9ad9bc66506e9d68ec86e3e44a0605ee665
Huynh Anh Quy <anhquy19042005@gmail.com>
2026-07-20 19:26:59 +0700

    feat(lobby): change the way to turn on/off panels

--- Assets/Scripts/UI/Lobby/LobbyUIController.cs
+++ Assets/Scripts/UI/Lobby/LobbyUIController.cs
@@ -1,89 +1,79 @@
-using UnityEngine;
 using DG.Tweening;
-using UnityEngine.UI;
+using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.UI;
 public class LobbyUIController : MonoBehaviour
 {
 
     [Header("Buttons")]
-    [SerializeField] private Button playBtn;
-    [SerializeField] private Button soundBtn;
-    [SerializeField] private Button easyModeBtn;
-    [SerializeField] private Button hardModeBtn;
-    [SerializeField] private Button backToLobbyBtn;
-    [SerializeField] private Button backSoundBtn;
+    [SerializeField] private Button _playBtn;
+    [SerializeField] private Button _settingBtn;
+    [SerializeField] private Button _easyModeBtn;
+    [SerializeField] private Button _hardModeBtn;
+    [SerializeField] private Button _backSelectBtn;
+    [SerializeField] private Button _backSoundBtn;
 
     [Header("Panels")]
-    [SerializeField] private GameObject soundMenu;
-    [SerializeField] private RectTransform menu;
-    [SerializeField] private RectTransform selectMode;
+    [SerializeField] private RectTransform _soundMenu;
+    [SerializeField] private RectTransform _menu;
+    [SerializeField] private RectTransform _selectMode;
 
     [Header("Event")]
     [SerializeField] private UnityEvent LoadEasySceneEvent;
     [SerializeField] private UnityEvent LoadHardSceneEvent;
 
-    [Header("Duration of Dotween")]
-    public float duration = 1;
-
-    private bool isTurnOnSoundMenu = false;
-    private Ease moveEase = Ease.OutQuart;
+    [Header("Move")]
+    public float _duration = 1;
+    private Ease _moveEase = Ease.OutQuart;
 
 
-    private void OnEnable(){
-        playBtn.onClick.AddListener(OpenSelectMode);
-        soundBtn.onClick.AddListener(TurnOnSoundMenu);
-        easyModeBtn.onClick.AddListener(()=>SelectMode(1));
-        hardModeBtn.onClick.AddListener(()=>SelectMode(2));
-        backToLobbyBtn.onClick.AddListener(BackToMenu);
-        backSoundBtn.onClick.AddListener(TurnOnSoundMenu);
-    }
-
-    private void OnDisable(){
-        playBtn.onClick.RemoveAllListeners();
-        soundBtn.onClick.RemoveAllListeners();
-        easyModeBtn.onClick.RemoveAllListeners();
-        hardModeBtn.onClick.RemoveAllListeners();
-        backToLobbyBtn.onClick.RemoveAllListeners();
-    }
-
-    
-
-    public void TurnOnSoundMenu()
+    private void OnEnable()
     {
-        if (!isTurnOnSoundMenu)
+        //main menu button
+        _playBtn.onClick.AddListener(() => ChangeComponentPos(_menu, new Vector2(0, -1100), _duration, _moveEase));
+        _playBtn.onClick.AddListener(() => ChangeComponentPos(_selectMode, new Vector2(0, -400), _duration, _moveEase));
+        _settingBtn.onClick.AddListener(() => ChangeComponentPos(_menu, new Vector2(0, 1100), _duration, _moveEase));
+        _settingBtn.onClick.AddListener(() => ChangeComponentPos(_soundMenu, new Vector2(0, 550), _duration, _moveEase));
+        //select mode button
+        _easyModeBtn.onClick.AddListener(() => SelectMode(1));
+        _hardModeBtn.onClick.AddListener(() => SelectMode(2));
+        _backSelectBtn.onClick.AddListener(() => ChangeComponentPos(_selectMode, new Vector2(0, 796), _duration, _moveEase));
+        _backSelectBtn.onClick.AddListener(() => ChangeComponentPos(_menu, Vector2.zero, _duration, _moveEase));
+        //sound menu button
+        _backSoundBtn.onClick.AddListener(() => ChangeComponentPos(_soundMenu, new Vector2(0, -400), _duration, _moveEase));
+        _backSoundBtn.onClick.AddListener(() => ChangeComponentPos(_menu, Vector2.zero, _duration, _moveEase));
+    }
+
+    private void OnDisable()
+    {
+        _playBtn.onClick.RemoveAllListeners();
+        _settingBtn.onClick.RemoveAllListeners();
+        _easyModeBtn.onClick.RemoveAllListeners();
+        _hardModeBtn.onClick.RemoveAllListeners();
+        _backSelectBtn.onClick.RemoveAllListeners();
+        _backSoundBtn.onClick.RemoveAllListeners();
+    }
+
+
+    public void ChangeComponentPos(RectTransform currComponent, Vector2 endPos, float duration, Ease moveEase)
+    {
+        currComponent.DOAnchorPos(endPos, duration).SetEase(moveEase).SetLink(gameObject);
+    }
+
+
+    private void SelectMode(int i)
+    {
+        if (i == 1)
         {
-            soundMenu.SetActive(true);
-            isTurnOnSoundMenu = true;
-        }
-        else
-        {
-            soundMenu.SetActive(false);
-            isTurnOnSoundMenu = false;
-        }
-    }
-
-    public void OpenSelectMode()
-    {
-        menu.DOAnchorPos(new Vector2(0,-1100), duration).SetEase(moveEase).SetLink(gameObject);
-        selectMode.DOAnchorPos(new Vector2(0, -395), duration).SetEase(moveEase).SetLink(gameObject);
-    }
-
-    public void BackToMenu()
-    {
-        selectMode.DOAnchorPos(new Vector2(0, 796), duration).SetEase(moveEase).SetLink(gameObject);
-        menu.DOAnchorPos(Vector2.zero, duration).SetEase(moveEase).SetLink(gameObject);
-    }
-
-    private void SelectMode(int i){
-        if(i==1){
             LoadEasySceneEvent?.Invoke();
         }
-        else if(i==2){
+        else if (i == 2)
+        {
             LoadHardSceneEvent?.Invoke();
         }
     }
 
-    
+
 
 
 }
