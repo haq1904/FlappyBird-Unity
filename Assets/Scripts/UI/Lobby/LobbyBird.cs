0d67006fc67256b7eda365137dbd893c6425bec4
Huynh Anh Quy <anhquy19042005@gmail.com>
2026-07-22 20:46:00 +0700

    feat(lobby): add shake and falling effect for bird(when it has collision with menu)

--- Assets/Scripts/UI/Lobby/LobbyBird.cs
+++ Assets/Scripts/UI/Lobby/LobbyBird.cs
@@ -3,18 +3,28 @@ using UnityEngine;
 
 public class LobbyBird : MonoBehaviour
 {
-    [Header("JumpAnchorPos fiels")]
-    [SerializeField] private float _jumpPower = 1;
-    [SerializeField] private int _numJumps = 1;
-    [SerializeField] private float _duration = 1;
+    [Header("LocalMoveY fiels")]
+    [SerializeField] private float _localMoveYDuration = 1;
+    [SerializeField] private Ease _localMoveYEase = Ease.InQuart;
+
+    [Header("ShakePosition fields")]
+    [SerializeField] private float _shakeDuration = 1;
+    [SerializeField] private Vector3 _shakeStrength;
+    [SerializeField] private int _shakeVibrato = 10;
+    [SerializeField] private float _shakeRandomness = 90;
+
 
     [Header("Fields")]
     [SerializeField] private Animator _animator;
+    [SerializeField] private RectTransform _menu;
 
     public void HandleCollision()
     {
         _animator.enabled = false;
-        float targetX = transform.localPosition.x - 800f;
-        transform.DOLocalJump(new Vector3(targetX, -2300, transform.localPosition.z), _jumpPower, _numJumps, _duration);
+        _menu.DOShakePosition(_shakeDuration, _shakeStrength, _shakeVibrato, _shakeRandomness).SetLink(gameObject);
+        transform.DOShakePosition(_shakeDuration, _shakeStrength, _shakeVibrato, _shakeRandomness).SetLink(gameObject).OnComplete(() =>
+        {
+            transform.DOLocalMoveY(-2300, _localMoveYDuration).SetEase(_localMoveYEase).SetLink(gameObject);
+        });
     }
 }
