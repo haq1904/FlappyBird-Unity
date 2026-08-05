888e1352e1471ba61fd17e38c1e2fc0ab6b90b34
Huynh Anh Quy <anhquy19042005@gmail.com>
2026-08-05 15:19:49 +0700

    feat(scene controller): use loading scene when changing scene

--- Assets/Scripts/Feature/Feature_GameFlow/Manager/SceneController.cs
+++ Assets/Scripts/Feature/Feature_GameFlow/Manager/SceneController.cs
@@ -1,30 +1,23 @@
 ﻿
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
+using Codice.Client.Common.GameUI;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using DG.Tweening;
-using Codice.Client.Common.GameUI;
-using System.Runtime.InteropServices.WindowsRuntime;
 
 public class SceneController : MonoBehaviour
 {
-    [SerializeField] private GameObject _loaderCanvas;
-    [SerializeField] private Image _progressBar;
-    [SerializeField] private bool IsEnableScript;
-    private float _target;
 
     public static SceneController Instance;
 
     private void Awake()
     {
-        if(Instance == null)
+        if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-            this.enabled=false;
-            IsEnableScript = this.enabled;
-            
         }
         else
         {
@@ -32,54 +25,22 @@ public class SceneController : MonoBehaviour
         }
     }
 
-   
+
     public async void LoadScene(int indexScene)
     {
-        _target = 0f;
-        _progressBar.fillAmount = 0f;
-
-        _loaderCanvas.SetActive(true);
 
         var scene = SceneManager.LoadSceneAsync(indexScene);
-        
+
+        LoadingScreenController.Instance.LoadingScrollDown(false);
+
         scene.allowSceneActivation = false;
 
-        do
-        {
-            await Task.Delay(100);
-            _target = scene.progress;
-        } while (scene.progress < 0.9f);
-         
-
-        await Task.Delay(1000);
+        await Task.Delay((int)LoadingScreenController.Instance.GetMinimumMoveDuration() * 1000);
 
         scene.allowSceneActivation = true;
 
-        while (!scene.isDone)
-        {
-            await Task.Yield();
-        }
-        _progressBar.fillAmount = 1f;
 
-        await Task.Delay(1000);
-        
-        _loaderCanvas.SetActive(false);
-        this.enabled = false;
-        IsEnableScript = this.enabled;
     }
 
-    public void EnableScript()
-    {
-        this.enabled = true;
-        IsEnableScript = this.enabled;
-        
-    }
 
-    private void Update()
-    {
-        _progressBar.fillAmount = Mathf.MoveTowards(_progressBar.fillAmount, _target, 3 * Time.deltaTime);
-        
-    }
-
-    
 }
