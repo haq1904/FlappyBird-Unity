a34b7d593556feddfd08f4f040b036c8c959898b
Huynh Anh Quy <anhquy19042005@gmail.com>
2026-08-27 19:04:05 +0700

    chore(pipe): shaking pipe instead of punching

--- Assets/Scripts/Feature/Feature_Obstacle/Pipe/Base/PartOfPipe.cs
+++ Assets/Scripts/Feature/Feature_Obstacle/Pipe/Base/PartOfPipe.cs
@@ -8,18 +8,18 @@ public class PartOfPipe: MonoBehaviour
     [SerializeField] private float _impactForce = 1;
     [SerializeField] private float _gravityScale = 1;
 
-    [Header("Punch")]
+    [Header("Shake")]
     [SerializeField] private float _duration = 1;
     [SerializeField] private Vector3 _streng;
     [SerializeField] private int _vibrato = 10;
-    [SerializeField,Range(0,1)] private float _elasticity  = 0;
+    [SerializeField,Range(0,180)] private float _randomness = 90;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.TryGetComponent<IDamageable>(out var damageableGameObject))
         {
             damageableGameObject.TakeDamage(Vector2.left, _impactForce, _gravityScale);
-            transform.DOPunchPosition(_streng, _duration, _vibrato, _elasticity).SetLink(gameObject);
+            transform.DOShakePosition(_duration, _streng, _vibrato, _randomness).SetLink(gameObject);
             Vector2 directionOfCollision = collision.GetContact(0).normal;
             if (directionOfCollision.x > 0.5f) //This condition force cracks to be spawned at the left side of catus
             {
